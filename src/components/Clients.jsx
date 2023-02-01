4c27e4cec885ed8ebdb21c5aa404ebbbc7900577
Pournima2496 <pournima2496@gmail.com>
2023-02-01 19:08:58 +0530

    react modern ui

---
+++ src/components/Clients.jsx
@@ -0,0 +1,18 @@
+import {clients} from '../constants'
+import styles from '../style'
+
+const Clients = () => (
+    <section id='clients' className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {
+          clients.map((client)=>(
+            <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] client-card cursor-pointer py-3 rounded-[5px]`} >
+              <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain' />
+            </div>
+          ))
+        }
+      </div>
+    </section>
+  )
+
+export default Clients
